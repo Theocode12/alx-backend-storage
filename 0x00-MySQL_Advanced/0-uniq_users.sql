30730c6a086a1f0792fbd0663da10cbb278f11c3
Theocode12 <vchidubem9@gmail.com>
2023-01-12 19:29:21 +0100

    no semi colon

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
--- script creates a table users
--- duplicate comment enforced
+-- Make an attribute unique directly in the table schema will enforced your business rules 
+--and avoid bugs in your application
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
 )
