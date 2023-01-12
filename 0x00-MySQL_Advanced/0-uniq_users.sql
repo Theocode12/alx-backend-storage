3182263c9f6c9d83eda795a2e69a14568f0b6bc7
Theocode12 <vchidubem9@gmail.com>
2023-01-12 19:29:46 +0100

    no semi colon

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Make an attribute unique directly in the table schema will enforced your business rules 
---and avoid bugs in your application
+-- and avoid bugs in your application
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
